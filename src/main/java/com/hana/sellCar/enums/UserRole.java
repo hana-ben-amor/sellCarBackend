71ad6dd4e255928831a87f6ac31f1ffbc39755c5
hana-ben-amor <hanabenamor2001@gmail.com>
2025-04-12 16:08:41 +0100

    first commit

---
+++ src/main/java/com/hana/sellCar/enums/UserRole.java
@@ -0,0 +1,5 @@
+package com.hana.sellCar.enums;
+
+public enum UserRole {
+    ADMIN,CUSTOMER
+}
---
+++ src/test/java/com/hana/sellCar/SellCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hana.sellCar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
